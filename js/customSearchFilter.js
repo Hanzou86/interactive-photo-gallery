8549cf1743bc07edf24760d3aee252a61ef663c7
James Albert <James.Albert1986@outlook.com>
2022-01-04 11:40:14 -0500

    update

---
+++ js/customSearchFilter.js
@@ -0,0 +1,19 @@
+// const gallery = []
+
+// function searchFilter() {
+//     let input, filter, gallery, image;
+//     input = document.getElementById('search');
+//     filter = input.value.toUpperCase();
+//     gallery = document.getElementsByClassName('gallery');
+//     image = gallery.getElementByTagName('a');
+
+//     for (i = 0; i < 12; i++) {
+
+//     }
+// }
+
+console.log(document.getElementById('search').addEventListener('keyup', searchValue ));
+
+function searchValue() {
+    
+}
